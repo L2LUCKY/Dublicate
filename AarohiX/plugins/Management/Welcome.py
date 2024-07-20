c922c7614512b19dac974e611c0c0450aba6b52a
ME KALIN BHAIYA <mdistkhar1078@gmail.com>
2024-07-20 13:42:46 +0530

    Update Welcome.py

--- AarohiX/plugins/Management/Welcome.py
+++ AarohiX/plugins/Management/Welcome.py
@@ -137,9 +137,9 @@ async def greet_group(_, member: ChatMemberUpdated):
 ┣ 𝟓 ➥ 𝐆ɪᴠᴇ  ʀᴇsᴘᴇᴄᴛ , ᴛᴀᴋᴇ  ʀᴇsᴘᴇᴄᴛ 
 ┗━━━━━━━━━━━━━━━━━      
 
-❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ 𝐐ᴜᴇᴇɴ ✘ 𝐌ᴜꜱɪᴄ
+❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [˹ 𝐕ᴇᴇɴᴀ ✘ 𝐌ᴜꜱɪᴄ ˼ ♪](https://t.me/VeenaMusic_bot)
 """,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/Queen_of_heart_music_bot?startgroup=true")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/VeenaMusic_bot?startgroup=true")]])
         )
     except Exception as e:
         LOGGER.error(e)
@@ -161,3 +161,4 @@ ID: {message.chat.id}
 USERNAME: @{message.chat.username}
 ➖➖➖➖➖➖➖➖➖➖➖➖
 """)
+
